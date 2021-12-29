fa2e4004ced18ca5d03c443a165b63c7e523abee
ProgNovel <prognovel@gmail.com>
2021-12-30 00:12:10 +0800

    adapter change

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-static";
 import autoprefixer from "autoprefixer";
 import preprocess from "svelte-preprocess";
 
